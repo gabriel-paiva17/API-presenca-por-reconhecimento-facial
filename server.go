b63c80edb16e1f0de03b28577037d61e3f221465
GabrielPaiva <gabrielanpaiva2005@gmail.com>
2024-05-25 14:36:02 -0300

    updating connectDB and router

--- server.go
+++ server.go
@@ -5,20 +5,29 @@ import (
 	"log"
 	"net/http"
 	"time"
+	"fmt"
 	//"myproject/user"
 
 	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 func CreateServer() {
 
-	client, err := connectDB()
+	// Conecta-se ao MongoDB
+	client, cancel, err := connectDB("mongodb://localhost:27017")
 	if err != nil {
-		log.Fatal("Error connecting to MongoDB:", err)
+		log.Fatal("Erro ao conectar ao MongoDB:", err)
 	}
-	defer client.Disconnect(context.Background())
+	defer func() {
+		
+		if err = client.Disconnect(context.Background()); err != nil {
+			log.Fatal("Erro ao desconectar do MongoDB:", err)
+		}
+		cancel() // Cancela o contexto para liberar recursos
+	}()
 
 	r := mux.NewRouter()
 
@@ -34,10 +43,25 @@ func CreateServer() {
 }
 
 
-func connectDB() (*mongo.Client, error) {
-	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI("mongodb://localhost:27017"))
+func connectDB(uri string) (*mongo.Client, context.CancelFunc, error) {
+	
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+
+	clientOptions := options.Client().ApplyURI(uri)
+
+	client, err := mongo.Connect(ctx, clientOptions)
 	if err != nil {
-		return nil, err
+		cancel()
+		return nil, nil, err
 	}
-	return client, nil
+
+	// Verifica se a conexão foi estabelecida com sucesso
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		cancel()
+		return nil, nil, err
+	}
+
+	fmt.Println("Conectado ao MongoDB!")
+	return client, cancel, nil
 }
