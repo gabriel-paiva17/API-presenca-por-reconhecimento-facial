76cca67927ac6122ee3e17467f6fbf982c25da82
GabrielPaiva <gabrielanpaiva2005@gmail.com>
2024-05-31 00:19:58 -0300

    defining member and group model

---
+++ group/model.go
@@ -0,0 +1,12 @@
+package group
+
+import (
+    "myproject/member" // Substitua "your_project_name" pelo nome real do seu módulo
+)
+
+type Group struct {
+    ID        string           `json:"id" bson:"_id"`
+    Name      string           `json:"name" bson:"name"`
+    CreatedAt string           `json:"createdAt" bson:"created_at"`
+    Members   []member.Member  `json:"members" bson:"members" `
+}
