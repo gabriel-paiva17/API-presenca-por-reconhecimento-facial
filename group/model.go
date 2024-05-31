2d166d0862464d605d797469a6cb173605e80ae0
GabrielPaiva <gabrielanpaiva2005@gmail.com>
2024-05-31 00:25:08 -0300

    renaming

--- group/model.go
+++ group/model.go
@@ -7,6 +7,6 @@ import (
 type Group struct {
     ID        string           `json:"id" bson:"_id"`
     Name      string           `json:"name" bson:"name"`
-    CreatedAt string           `json:"createdAt" bson:"created_at"`
+    CreatedAt string           `json:"createdAt" bson:"createdAt"`
     Members   []member.Member  `json:"members" bson:"members" `
 }
