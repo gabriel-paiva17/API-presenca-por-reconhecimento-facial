f4cdfbb52cb70816cf12455a5356308a31df4cd2
GabrielPaiva <gabrielanpaiva2005@gmail.com>
2024-05-31 00:35:17 -0300

    spacing

--- member/model.go
+++ member/model.go
@@ -4,6 +4,6 @@ type Member struct {
     ID          string `json:"id" bson:"_id"`
     Name        string `json:"name" bson:"name"`
     Face        string `json:"face" bson:"face"`
-    Attendance   int    `json:"attendance" bson:"attendance"`
-    AddedAt string `json:"addedAt" bson:"addedAt"`
+    Attendance  int    `json:"attendance" bson:"attendance"`
+    AddedAt     string `json:"addedAt" bson:"addedAt"`
 }
