c3f2e1f3d235f00443fe5fe680ad7537435e4d41
GabrielPaiva <gabrielanpaiva2005@gmail.com>
2024-05-31 01:05:29 -0300

    starting group repo

--- member/model.go
+++ member/model.go
@@ -7,3 +7,13 @@ type Member struct {
     Attendance  int    `json:"attendance" bson:"attendance"`
     AddedAt     string `json:"addedAt" bson:"addedAt"`
 }
+
+type CreateGroupRequest struct {
+    Name string `json:"name"`
+}
+
+type CreateGroupResponse struct {
+    ID        string    `json:"id"`
+    Name      string    `json:"name"`
+    CreatedAt string    `json:"createdAt"`
+}
